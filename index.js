e37a75e17b8ec4d1e74da55ee7d323e4ae2dc58e
Leo McArdle <leomcardle@gmail.com>
2015-06-24 18:33:34 +0100

    ( ͡° ͜ʖ ͡°) ¯\_(ツ)_/¯

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var cm = require('sdk/context-menu')
 
+var lennys = ['( ͡° ͜ʖ ͡°)', '¯\\_(ツ)_/¯']
+
+var items = []
+lennys.forEach(function (lenny) {
+  var item = cm.Item({
+    label: lenny,
+    data: lenny
+  })
+  items.push(item)
+})
+
 cm.Menu({
   label: '( ͡° ͜ʖ ͡°)',
   context: cm.SelectorContext('input, textarea, [contentEditable]'),
   contentScriptFile: './context.js',
-  items: [
-    cm.Item({
-      label: '( ͡° ͜ʖ ͡°)',
-      data: '( ͡° ͜ʖ ͡°)'
-    }),
-    cm.Item({
-      label: '¯\\_(ツ)_/¯',
-      data: '¯\\_(ツ)_/¯'
-    })
-  ]
+  items: items
 })
